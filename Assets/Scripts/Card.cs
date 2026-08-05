7290a7e49ce3269250b4181270920ebfe6841d00
seo <seoseoseo11070@gmail.com>
2026-08-05 18:35:07 +0900

    NPC追加

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,17 @@
+public class Card
+{
+    public enum SuitType { Spade, Heart, Diamond, Club, Joker }
+
+    public int id;
+    public SuitType suit;
+    public int number;
+    public int strength;
+
+    public Card(int id, SuitType suit, int number, int strength)
+    {
+        this.id = id;
+        this.suit = suit;
+        this.number = number;
+        this.strength = strength;
+    }
+}
